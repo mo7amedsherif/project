d99356d7cbd599d27fa08fcef47a0e9a46387f00
mo7amedsherif <160414123+mo7amedsherif@users.noreply.github.com>
2024-12-17 04:15:38 +0200

    add file

--- lib/main.dart
+++ lib/main.dart
@@ -9,80 +9,191 @@ class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  MyAppState createState() =>MyAppState();
+  MyAppState createState() => MyAppState();
 }
-class MyAppState extends State<MyApp>{
-  final AudioPlayer _player = AudioPlayer();
-  Future<void> playSound() async {
-    await _player.play(AssetSource('audio/mo3.mp3'));
+
+class MyAppState extends State<MyApp> {
+  final List<AudioPlayer> _player = List.generate(6, (_) => AudioPlayer());
+  final List<bool> isPlaying = List.generate(6, (_) => false);
+
+  Future<void> playSound(int index, String audiopath) async {
+    if (!isPlaying[index]) {
+      await _player[index].play(AssetSource(audiopath));
+    }
+
+    setState(() {
+      isPlaying[index] = true;
+    });
   }
 
   // إيقاف الصوت
-  Future<void> stopSound() async {
-    await _player.stop();
+  Future<void> stopSound(int index) async {
+    await _player[index].stop();
+    setState(() {
+      isPlaying[index] = false;
+    });
   }
+
   @override
   void dispose() {
-    _player.dispose();
+    for (var player in _player) {
+      player.dispose();
+    }
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 154, 215, 210),
-        
+        backgroundColor: const Color.fromARGB(238, 81, 87, 90),
         appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(168, 150, 90, 1),
-          title: const Text('iBookCast'),
-          //iconTheme: Icon(Icons.headphones_rounded,color:Color.fromRGBO(206, 82, 60,1),),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          backgroundColor: const Color.fromARGB(255, 42, 43, 43),
+          title: const Row(
             children: [
-              Image.asset('assets/image/mo.jpg',width: 50,height: 100,),
-              // ignore: prefer_const_constructors
-              Text('book1',
-              textAlign: TextAlign.center,
-              textScaler: const TextScaler.linear(2),
-              selectionColor: Colors.deepOrange,
+              Icon(
+                Icons.headphones,
+                color: Colors.amber,
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(onPressed:playSound,
-              style: ElevatedButton.styleFrom(
-                backgroundColor:Colors.green),
-              child:const Text('Play'),
+              SizedBox(
+                width: 5,
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(onPressed:stopSound,
-              style: ElevatedButton.styleFrom(
-                backgroundColor:Colors.red),
-              child:const Text('stop'),
+              Text(
+                'iBookCast',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 210,
               ),
             ],
-            
           ),
+          actions: [
+            Icon(
+              Icons.menu_rounded,
+              color: Colors.amber[400],
+            ),
+          ],
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(10),
+          children: [
+            _audiosourc(
+                0, 'assets/image/mo.jpg', 'assets/audio/mo1.mp3', 'book1'),
+            const SizedBox(height: 20),
+            _audiosourc(
+                1, 'assets/image/mo.jpg', 'assets/audio/mo2.mp3', 'book1'),
+            const SizedBox(height: 20),
+            _audiosourc(
+                2, 'assets/image/mo.jpg', 'assets/audio/mo1.mp3', 'book1'),
+            const SizedBox(height: 20),
+            _audiosourc(
+                3, 'assets/image/mo.jpg', 'assets/audio/mo1.mp3', 'book1'),
+            const SizedBox(height: 20),
+            _audiosourc(
+                4, 'assets/image/mo.jpg', 'assets/audio/mo1.mp3', 'book1'),
+            const SizedBox(height: 20),
+            _audiosourc(
+                5, 'assets/image/mo.jpg', 'assets/audio/mo1.mp3', 'book1'),
+            const SizedBox(height: 20),
+            _audiosourc(
+                6, 'assets/image/mo.jpg', 'assets/audio/mo1.mp3', 'book1'),
+            const SizedBox(height: 20),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 42, 43, 43),
+          selectedItemColor: const Color.fromARGB(255, 255, 255, 255),
+          unselectedItemColor: Colors.amber[400],
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Account',
+            ),
+          ],
         ),
       ),
     );
   }
 
+  Widget _audiosourc(int index, String imagepath, String audiopath, String name) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 98, 118, 123),
+        borderRadius: BorderRadius.circular(40),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 10,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            imagepath,
+            width: 90,
+            height: 90,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 10,
+            width: 50,
+          ),
+          Text(
+            name,
+            style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 253, 254, 254)),
+          ),
+          const SizedBox(
+            height: 10,
+            width: 30,
+          ),
+          const Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => playSound(index, audiopath),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255,255, 202, 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  ),
+                child: const Icon(
+                  Icons.play_circle_outline,
+                  color: Colors.lightGreen,
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () => stopSound(index),
+                style: ElevatedButton.styleFrom(backgroundColor: const Color.fromARGB(255,255, 202, 40)),
+                child: const Icon(
+                  Icons.stop_circle_outlined,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
-//  child: GestureDetector(
-//           onTap: () {
-//             AudioPlayer().play(AssetSource('audio/mo3.mp3'));
-//           },
-//           onDoubleTap: () {
-//             AudioPlayer().stop();
-//           },
-//           child: ElevatedButton.icon(
-//               onPressed: null,
-//               icon: Image.asset(
-//                 'assets/image/mo.jpg',
-//                 width: 200,
-//                 height: 100,
-//               ),
-//               label: const Text('book1')),
-//         )
